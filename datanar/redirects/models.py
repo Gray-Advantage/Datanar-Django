d24ff04fdb05b97fdacf6d241b4b067784749af1
Gray-Advantage <103335340+Gray-Advantage@users.noreply.github.com>
2023-12-03 01:19:37 +0300

    style: flake8 + black

--- datanar/redirects/models.py
+++ datanar/redirects/models.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 
 class RedirectManager(models.Manager):
@@ -18,9 +18,9 @@ class RedirectManager(models.Manager):
 
         if all_good:
             return redirect
-        else:
-            redirect.delete()
-            redirect.save()
+
+        redirect.delete()
+        redirect.save()
 
         return None
 
