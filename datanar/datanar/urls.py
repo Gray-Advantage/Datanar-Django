e970316f5c48b7ab3ab9453e27ec06c591dc5227
bzorn <v.klimenko.2137@gmail.com>
2023-12-08 19:21:33 +0300

    feat: added header and theme toggle

--- datanar/datanar/urls.py
+++ datanar/datanar/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -15,6 +16,9 @@ if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns.append(path("__debug__/", include(debug_toolbar.urls)))
+    urlpatterns.append(
+        *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+    )
 
 
 __all__ = [urlpatterns]
