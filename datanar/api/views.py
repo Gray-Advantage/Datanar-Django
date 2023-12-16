f6674feac784264cd3e09d8c304ee459b59818f8
bzorn <v.klimenko.2137@gmail.com>
2023-12-16 21:00:51 +0300

    feat: black

--- datanar/api/views.py
+++ datanar/api/views.py
@@ -90,7 +90,9 @@ class RedirectViewSet(viewsets.ViewSet, mixins.CreateModelMixin):
         serializer.save(**serializer_data)
         headers = self.get_success_headers(serializer.data)
         return Response(
-            serializer.data, status=status.HTTP_201_CREATED, headers=headers,
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=headers,
         )
 
 
