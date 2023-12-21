da72915cb2694fdb825b910899f2e2a872ce7d21
Gray-Advantage <103335340+Gray-Advantage@users.noreply.github.com>
2023-12-21 04:40:00 +0300

    chore: add verbose_name to apps

--- datanar/statistic/apps.py
+++ datanar/statistic/apps.py
@@ -1,9 +1,12 @@
 from django.apps import AppConfig
+from django.utils.functional import lazy
+from django.utils.translation import gettext_lazy as _
 
 
 class StatisticConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "statistic"
+    verbose_name = lazy(lambda: _("statistic").capitalize(), str)
 
 
 __all__ = []
