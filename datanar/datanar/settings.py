90edbbfa6d39ca7fbc778448d0a3bdf2585192f9
Gray Advantage <sergeyindra2006@gmail.com>
2024-06-13 00:39:13 +0300

    feat: new version

--- datanar/datanar/settings.py
+++ datanar/datanar/settings.py
@@ -7,7 +7,7 @@ from django.utils.translation import gettext_lazy as _
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-VERSION = "1.6.1"
+VERSION = "1.6.2"
 
 SECRET_KEY = config(
     "DJANGO_SECRET_KEY",
