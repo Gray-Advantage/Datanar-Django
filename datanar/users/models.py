512e7d0710eeef7f038d97dcbded9eea4f4f74d0
Gray Advantage <sergeyindra2006@gmail.com>
2024-08-16 04:33:18 +0300

    feat: Redesigned user profile page + added thumbnails instead of real-size avatars

--- datanar/users/models.py
+++ datanar/users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from sorl.thumbnail import delete, get_thumbnail
 
 
 class User(AbstractUser):
@@ -13,6 +14,18 @@ class User(AbstractUser):
 
     def has_avatar(self):
         return self.avatar and self.avatar.url is not None
+
+    def get_small_avatar(self):
+        return get_thumbnail(self.avatar, "80", crop="center").url
+
+    def get_large_avatar(self):
+        return get_thumbnail(self.avatar, "200", crop="center").url
+
+    def save(self, *args, **kwargs):
+        old = User.objects.get(pk=self.pk)
+        if old.has_avatar() and not self.has_avatar():
+            delete(old.avatar)
+        super().save(*args, **kwargs)
 
 
 __all__ = [User]
