9bed875ef5e59e12a8297dc86b706a4a672a4ce7
Gray Advantage <sergeyindra2006@gmail.com>
2024-08-20 18:17:11 +0300

    feat: add info about activate status in redirect detail

--- datanar/static_dev/js/statistic/chartStatistic.js
+++ datanar/static_dev/js/statistic/chartStatistic.js
@@ -1,3 +1,6 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))  // включить подсказки
+
 const browserCanvas = document.getElementById("browserChart").getContext("2d");
 const browserData = document.querySelectorAll('#browserChart input[type="hidden"]');
 
