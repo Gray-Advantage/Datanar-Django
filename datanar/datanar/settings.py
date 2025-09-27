2fa01da0f0a9487ce3164d81c1f6bb27de90fbe2
bzorn <v.klimenko.2137@gmail.com>
2025-09-27 15:51:15 +0300

    feat: add django.contrib.sites app

--- datanar/datanar/settings.py
+++ datanar/datanar/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
     "django_cleanup",
     "sorl.thumbnail",
     "django_user_agents",
